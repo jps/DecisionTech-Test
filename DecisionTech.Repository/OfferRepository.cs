5a9b5b8e0338689f8f431e5f9f1aa90aa18201a2
jps <me@jspenc.com>
2016-02-17 20:42:11 +0000

    offer seed data.

--- DecisionTech.Repository/OfferRepository.cs
+++ DecisionTech.Repository/OfferRepository.cs
@@ -5,18 +5,30 @@ namespace DecisionTech.Repository
 {
     public class OfferRepository : InMemoryRepository<Offer, int>, IOfferRepository
     {
-        //I'm cheating as bit here just to init the data set generally I would inject another repo in here
+        //I'm cheating as bit here just to init the data set generally I wouldn't ever inject another repo in here
         public OfferRepository(IProductRepository productRepository)
         {
-
-            //productRepository.ge
-
+            
+            var butter = productRepository.GetByName("Butter");
+            var bread = productRepository.GetByName("Bread");
+            var milk = productRepository.GetByName("Milk");
+            //Buy 2 Butter and get a Bread at 50% off
             Collection.Add(new Offer()
             {
-                
+                PurchaseProduct = butter,
+                DiscountProduct = bread,
+                PercentDiscount = 0.50f,
+                RequiredPurchaseQuantity = 2
             });
 
-
+            //Buy 3 Milk and get the 4th milk for free
+            Collection.Add(new Offer()
+            {
+                PurchaseProduct = milk,
+                DiscountProduct = milk,
+                PercentDiscount = 1.00f,
+                RequiredPurchaseQuantity = 3
+            });
         }
     }
 }
